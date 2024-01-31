93a0afc37e6c4bfa9f121bc9ec05edf86c18ace8
Andrii Bui <andriibui@microsoft.com>
2024-01-31 22:07:26 +0100

    added function logic

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,25 @@
 const { onRequest } = require("firebase-functions/v2/https");
 const { initializeApp } = require("firebase-admin/app");
+const { firestore } = require("firebase-admin");
 
 initializeApp();
 
 exports.handleRequest = onRequest(async (req, res) => {
   console.log('[handleRequest] running inside the http method');
 
-  console.log(req.query.text);
-  
-  res.json({ result: `OK` });
-});
+  const collectionName = 'your_collection_name';
 
+  const documentData = {
+    query: req.query.text
+  };
+
+  try {
+    const collectionRef = firestore().collection(collectionName);
+    await collectionRef.add(documentData);
+
+    res.json({ result: `Document added to Firestore` });
+  } catch (error) {
+    console.error('Error adding document: ', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
